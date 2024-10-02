baa5b61bc3cf0e81878a355c8c3986718acb1794
ITÔ Hiroki <abies.firma@gmail.com>
2024-10-03 08:09:54 +0900

    update TMBモデルファイルの追加 custom.scssの追加

--- models/poismix.cpp
+++ models/poismix.cpp
@@ -11,6 +11,7 @@ Type objective_function<Type>::operator() ()
   PARAMETER_VECTOR(epsilon); // Random effect
   PARAMETER(log_sigma);      // log SD of random effect
   Type nll = 0;              // negative log likelihood
+
   nll += -sum(dnorm(epsilon, Type(0.0), exp(log_sigma), true));
   for (int i = 0; i < Y.size(); i++) {
     Type lambda = exp(alpha + beta * X(i) + epsilon(G(i)));
