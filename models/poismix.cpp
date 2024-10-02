6908dd0922f82671bab2f048e990bda3c8c21705
ITÔ Hiroki <abies.firma@gmail.com>
2024-10-02 13:59:03 +0900

    update Kanazawa.Rの宣伝 モデルコード

---
+++ models/poismix.cpp
@@ -0,0 +1,20 @@
+#include <TMB.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_VECTOR(Y);            // Objective variable
+  DATA_VECTOR(X);            // Explanatory variable
+  DATA_IVECTOR(G);           // Group index
+  PARAMETER(alpha);          // Intercept
+  PARAMETER(beta);           // Slope
+  PARAMETER_VECTOR(epsilon); // Random effect
+  PARAMETER(log_sigma);      // log SD of random effect
+  Type nll = 0;              // negative log likelihood
+  nll += -sum(dnorm(epsilon, Type(0.0), exp(log_sigma), true));
+  for (int i = 0; i < Y.size(); i++) {
+    Type lambda = exp(alpha + beta * X(i) + epsilon(G(i)));
+    nll += -dpois(Y(i), lambda, true);
+  }
+  return nll;
+}
